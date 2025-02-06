f31130bb34054cc642482cc674badbb419525e2c
cptBeardo <cam.mic.sag@gmail.com>
2025-02-06 09:42:59 -0600

    printed word count for Frankenstein.txt

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        word_list = file_contents.split()
+        word_count = len(word_list)
+        print(word_count)
+
+main()
