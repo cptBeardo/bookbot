299a63ca8c48c03becdaacc753167a1dab9b9bda
cptBeardo <cam.mic.sag@gmail.com>
2025-02-06 23:08:32 -0600

    final code before submitting on "Print a Report" step.

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ def count_characters(file_contents):
                 character_dict[character] += 1
             else: 
                 character_dict[character] = 1
+    return character_dict
 
 def convert_to_sorted_list(character_dict):
     char_list = []
     for char, count in character_dict.items():
         char_list.append({"char": char, "num": count})
     
-    # Sort the list by frequency
     char_list.sort(reverse=True, key=sort_on)
     return char_list
 
@@ -38,51 +38,9 @@ def main():
         file_contents = f.read()
 
     word_count = get_word_count(file_contents)
-        # word_list = file_contents.split()
-        # word_count = len(word_list)
-        # print(f"{word_count} words")
-        # character_set = set(file_contents.lower())
-        # print(character_set)
-        # character_list = sorted(list(character_set))
-        # print(character_list)
     character_dict = count_characters(file_contents)
-        """ character_dict = {}
-        for character in file_contents.lower():
-            if character.isalpha():
-                if character in character_dict:
-                    character_dict[character] += 1
-                else:
-                    character_dict[character] = 1"""
-
-
-        """print("--- Begin report of books/frankenstein.txt ---")
-        print(f"{word_count} words foun in the document\n")"""
-
     char_list = convert_to_sorted_list(character_dict)
-    # convert dictionary to list of dictionaries
-    """char_list = []
-    for char, count in character_dict.items():
-        char_list.append({"char": char, "num": count})
-    
-    # Sort the list by frequency
-    char_list.sort(reverse=True, key=sort_on)"""
-    
-    
     print_report(word_count, char_list)
-    # Print report
-    """print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{word_count} words found in the document\n")
-    
-    # Print each character count
-    for char_info in char_list:
-        print(f"The '{char_info['char']}' character was found {char_info['num']} times")
-    
-    print("--- End report ---")
-"""
-        # alphabet = character_list[-26:]
-        # print(alphabet)
-
-
 
 
 main()
